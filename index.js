0aaa99a8a337767984615e907ca90b9595cb0da3
Abdullah Husein <abdullah.husein9192@gmail.com>
2019-04-20 08:22:40 +0700

    Change to express

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+// const server = http.createServer((request, response) => {
+//    response.writeHead(200, {"Content-Type": "text/plain"});
+//    response.end("Hello World!");
+// });
 
+//const port = process.env.PORT || 1337;
+//server.listen(port);
+
+//console.log("Server running at http://localhost:%d", port);
+
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 1337;
-server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
